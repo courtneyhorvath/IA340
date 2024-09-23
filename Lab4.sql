b8f5b8985f2762589368bc40f645ce3ba5990295
courtneyhorvath <courthorvath20@gmail.com>
2024-09-23 16:20:36 -0400

    Update Lab4.sql

--- Lab4.sql
+++ Lab4.sql
@@ -7,8 +7,14 @@
 --4.3
 /* The ChatGPT responses are pretty good but not perfect */
 
+--4.4 
+/* ER diagram downloaded from PgAdmin */ 
+
+--4.5 
+/* ChatGPT can recognize the ER diagram and provided a breakdown */
+
 --4.6 
-/* it worked! */
+/* it worked! District of Columbia, 2019 */
 SELECT name.name, income.income, income.year
 FROM income
 JOIN name ON income.fips = name.fips
@@ -20,8 +26,27 @@ LIMIT 1;
 
 --4.7 
 /*prompt: Write your prompt to calculate the VA population growth rate in the past five years.*/ 
-SELECT population
+SELECT 
+recent_data.year AS recent_year, 
+recent_data.pop AS recent_population,
+old_data.year AS five_years_ago,
+old_data.pop AS population_five_years_ago, 
+((recent_data.pop - old_data.pop) / old_data.pop) * 1 
+FROM 
+(SELECT pop, year
 FROM population
 WHERE fips = '51'
-AND year = (SELECT MAX(year) - 5 FROM population)
-/* prompt needs to be adjusted */ 
+ORDER BY year DESC
+LIMIT 1) AS recent_data 
+JOIN 
+(SELECT pop, year
+FROM population
+WHERE fips = '51'
+ORDER BY year DESC 
+OFFSET 5
+LIMIT 1) AS old_data
+ON 1 = 1;
+/* I modified the prompt to give a more accurate guidline */ 
+
+--4.8 
+/* ChatGPT gives accurate responses most of the time. Sometimes the prompt needs to be altered in order to answer the question fully */ 
